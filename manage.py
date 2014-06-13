3d41b35e78bea4e8ff0228a5a3bcd4e6400fb3b5
rkktateishi <rkktateishi@gmail.com>
2014-06-13 16:40:24 -0600

    Added new settings configuration to remove database information from repo

--- manage.py
+++ manage.py
@@ -2,8 +2,20 @@
 import os
 import sys
 
+def here(*paths):
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), *paths))
+
+if not hasattr(sys, 'real_prefix'):
+    activate_this = here('bin/activate_this.py')
+    execfile(activate_this, dict(__file__=activate_this))
+
+path = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, here())
+sys.path.insert(0, here('DungeonMaster'))
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DungeonMaster.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", 
+            "DungeonMaster.settings")
 
     from django.core.management import execute_from_command_line
 
